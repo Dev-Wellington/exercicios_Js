65a9b82001012a36667c3d2258ee0f2a6570718c
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-10-05 18:24:34 -0300

    feat: Função de validar input finalizada

--- projeto_pratico/assets/js/validarInputs.js
+++ projeto_pratico/assets/js/validarInputs.js
@@ -3,13 +3,16 @@ const erro = document.querySelector(".form__error");
 const btnSubmit = document.querySelector(".form__button");
 erro.style.visibility = "hidden";
 
-input.addEventListener("blur", () => {
-  if (parseFloat(input.value) < 1 || isNaN(parseFloat(input.value))) {
-    erro.style.visibility = "visible";
-    btnSubmit.disabled = true;
-  } else {
-    erro.style.visibility = "hidden";
-    btnSubmit.disabled = false;
+const validarInput = (e) => {
+  input.addEventListener("blur", () => {
+    if (parseFloat(input.value) < 1 || isNaN(parseFloat(input.value))) {
+      erro.style.visibility = "visible";
+      btnSubmit.disabled = true;
+    } else {
+      erro.style.visibility = "hidden";
+      btnSubmit.disabled = false;
+    }
+  });
+};
 
-  }
-});
+export { validarInput };
